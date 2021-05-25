94c81ec6b1924e4e00f04a8184d23dae9f14c013
arm <arm@arm.arm>
2021-05-25 19:58:16 +0500

    - Убран старый неиспользуемый код (plic).

--- src/syslib/interrupts/interrupt.h
+++ src/syslib/interrupts/interrupt.h
@@ -12,40 +12,9 @@
 
 
 //Interrupt common
-
 extern "C" void SetHandler(void (*ptrHandler)(void));
 extern "C" void SetMepc(void (*ptrReturn)(void));
 extern "C" void handler(void) __attribute__((interrupt, aligned(4)));
-
-
-////////////////
-
-///Убрать!!!
-
-extern inline void __attribute__((always_inline))  get_interrupt_diag(void);
-
-extern inline void __attribute__((always_inline)) void_interrupt_compleat(void);
-
-void interrupt_compleat(void);
-
-
-void EnableExternInterrupt(void);
-void DisableExternInterrupt(void);
-
-/**
- * Диагностика при возникновении прерывания/исключения (глобально в секции .diag_section)
- */
-typedef struct
-{
-	unsigned long int mcause;
-	unsigned long int mepc;
-	unsigned long int mtval;
-	unsigned long int mie;
-	unsigned long int mip;
-
-}_interrupt_diag;
-
-//==================================================
 
 
 #endif /* INTERRUPT_H_ */
